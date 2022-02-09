0bfaad97594bda886808b64fdf97919b2a3d764f
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-02-08 19:17:34 -0600

    First Commit

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+
+
+let yourName = "Melissa Zuengler" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -13,15 +15,74 @@ const credit = document.querySelector('#credit')
 // selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
 
+const gbMinusBtn = document.querySelector('#minus-gb')
+
+const ccPlusBtn = document.querySelector('#add-cc')
+
+const ccMinusBtn = document.querySelector('#minus-cc')
+
+const sugarPlusBtn = document.querySelector('#add-sugar')
+
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    gb = gb+1
+    console.log(gb)
+    document.getElementById("qty-gb").innerHTML = gb;
+    document.getElementById("qty-total").innerHTML = gb+cc+sugar;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+gbMinusBtn.addEventListener('click', function() {
+    
+    if (gb>=1)
+        {gb = gb-1
+        console.log(gb)
+        document.getElementById("qty-gb").innerHTML = gb;
+     document.getElementById("qty-total").innerHTML = gb+cc+sugar;}
+})
+
+ccPlusBtn.addEventListener('click', function() {
+
+    cc = cc+1
+    console.log(cc)
+    document.getElementById("qty-cc").innerHTML = cc;
+    document.getElementById("qty-total").innerHTML = gb+cc+sugar;
+})
+
+ccMinusBtn.addEventListener('click', function() {
+
+
+    if (cc>=1)
+        {cc = cc-1
+        console.log(cc)
+        document.getElementById("qty-cc").innerHTML = cc;
+        document.getElementById("qty-total").innerHTML = gb+cc+sugar;}
+
+})
+
+sugarPlusBtn.addEventListener('click', function() {
+
+    sugar = sugar+1
+    console.log(sugar)
+    document.getElementById("qty-sugar").innerHTML = sugar;
+    document.getElementById("qty-total").innerHTML = gb+cc+sugar;
+})
+
+sugarMinusBtn.addEventListener('click', function() {
+    
+    if (sugar>=1)
+    {sugar = sugar-1
+    console.log(sugar)
+    document.getElementById("qty-sugar").innerHTML = sugar;
+    document.getElementById("qty-total").innerHTML = gb+cc+sugar;}
+})
+
+
+
+
+// TODO: Hook up event listeners for the rest of the button
